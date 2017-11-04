7c0f0e260a3a8afe79dd052bb0edf82482118161
McCorriston, Jeromy <Jeromy_McCorriston@PremierInc.com>
2017-11-04 17:56:46 -0400

    Created project

---
+++ Gathering/Gathering/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gathering.Startup))]
+namespace Gathering
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
