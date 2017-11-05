7e5e4b38be0ef591dbf7ff557821d5cada071a9b
Allen Hazlett <allendwighthazlett@yahoo.com>
2017-11-05 02:12:29 -0500

    Added Authorize and autoredirect if not logged in.

--- Gathering/Gathering/Controllers/CourseController.cs
+++ Gathering/Gathering/Controllers/CourseController.cs
@@ -7,15 +7,24 @@ using System.Web.Mvc;
 
 namespace Gathering.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private TeacherService tService = new TeacherService();
         private StudentService sService = new StudentService();
         private CourseService cService = new CourseService();
 
+        public ActionResult Index()
+        {
+            ViewBag.AllCourses = true;
+            ViewBag.Courses = cService.GetAll();
+            return View();
+        }
+
         // GET: Course
         public ActionResult Index(int id)
         {
+            ViewBag.AllCourses = false;
             ViewBag.Course = cService.Get(id);
             return View();
         }
