e71cd3b422a91bafbbaf583127adff5f8812d210
Allen Hazlett <allendwighthazlett@yahoo.com>
2017-11-05 09:52:08 -0500

    BEFORE MERGE

---
+++ Gathering/Gathering/Services/GradeService.cs
@@ -0,0 +1,43 @@
+﻿using Gathering.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gathering.Services
+{
+    public class GradeService : Service
+    {
+        #region RESTful Methods
+        public List<Grade> GetAll()
+        {
+            return this.GetAll<Grade>();
+        }
+
+        public Grade Get(int id)
+        {
+            return this.GetAll<Grade>().First(t => t.Id == id);
+        }
+
+        public void Create(Grade obj)
+        {
+            this.Create<Grade>(obj);
+        }
+
+        public void Update(Grade obj)
+        {
+            var newObj = this.db.Grades.First(t => t.Id == obj.Id);
+            newObj = obj;
+            this.db.SaveChanges();
+        }
+
+        public void Delete(Grade obj)
+        {
+            this.Delete<Grade>(obj);
+        }
+        #endregion
+
+        #region Other
+        #endregion
+    }
+}
