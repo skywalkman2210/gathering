789695f8aa07dd120f0fcebe6c6bc46248d6fdab
McCorriston, Jeromy <Jeromy_McCorriston@PremierInc.com>
2017-11-05 06:47:44 -0500

    them css changes tho

--- Gathering/Gathering/Controllers/AccountController.cs
+++ Gathering/Gathering/Controllers/AccountController.cs
@@ -33,7 +33,7 @@ namespace Gathering.Controllers
             this.teacherService = new TeacherService();
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -45,9 +45,9 @@ namespace Gathering.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -95,7 +95,7 @@ namespace Gathering.Controllers
 
                     var role = user.AspNetRoles.First();
 
-                    switch(role.Id)
+                    switch (role.Id)
                     {
                         case "2":
                             UserSession.RoleType = RoleType.Teacher;
@@ -150,7 +150,7 @@ namespace Gathering.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
