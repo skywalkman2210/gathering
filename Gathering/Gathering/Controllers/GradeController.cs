9aaa664b8799c491177aa0f3673c033db666f4b0
Allen Hazlett <allendwighthazlett@yahoo.com>
2017-11-05 02:41:33 -0500

    Passing course into view

--- Gathering/Gathering/Controllers/GradeController.cs
+++ Gathering/Gathering/Controllers/GradeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gathering.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,11 @@ namespace Gathering.Controllers
 {
     public class GradeController : Controller
     {
-        public ActionResult Index()
+        private CourseService cService = new CourseService();
+
+        public ActionResult Index(int id)
         {
+            ViewBag.Course = cService.Get(id);
             return View();
         }
     }
